c51000b37c45a824a28f6ebd2aedcc1f0d4b0722
PaulissenPieter <pieter.paulissen@student.uhasselt.be>
2020-12-21 10:13:00 +0100

    html

---
+++ resources/views/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>De jupiler pro league</title>
+    </head>
+    <style>
+        h1 {text-align: center;} 
+    </style>
+    <body>
+        <header>
+            <h1>
+                <img style="float:left" width="170" height="120" src="https://i.postimg.cc/Y0Zn6WHj/107489487-10157931648698451-7171084576164827977-o-jpg-nc-cat-108-ccb-2-nc-sid-8024bb-nc-ohc-TXROM.jpg" alt="logo" />
+                De jupkes league, voor bier en voetbal liefhebbers
+                <img style="float:right" src="https://i.postimg.cc/bvNSpV7H/jupiler-fles-33cl-1-1.jpg" alt="logo" />
+            </h1>
+        </header>
+        
+    </body>
+</html>
